44e5a494c547001c4e6e68f1364b9f0528ca1ffc
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-01-02 10:10:37 +0100

    Exercises 05

---
+++ exercises/exercises-05.go
@@ -0,0 +1,20 @@
+package main
+
+import ("fmt")
+
+type myType int
+var x myType
+var y int
+
+
+func main()  {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	x = 42
+	fmt.Println(x)
+
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+}
