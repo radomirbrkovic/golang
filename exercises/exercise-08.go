f0687293574a4ec865a63a64373fc42d98d49b33
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-01-04 09:24:24 +0100

    exercise 8

---
+++ exercises/exercise-08.go
@@ -0,0 +1,13 @@
+// Create TYPED and UNTYPED constants. Print the values of the constants.
+
+package main 
+import "fmt"
+
+const (
+	a = 42
+	b int = 21
+)
+
+func main()  {
+	fmt.Println(a, b)
+}
