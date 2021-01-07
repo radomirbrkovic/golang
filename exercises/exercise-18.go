177b83cf529054e6aaaa7a9bd28baac8d5b2a983
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-01-07 10:54:32 +0100

    exercises fix

--- exercises/exercise-18.go
+++ exercises/exercise-18.go
@@ -12,7 +12,7 @@ package main
 import "fmt"
 
 func main()  {
-	x := []int {42, 43, 44, 45, 46, 47, 48, 49, 51, 52}
+	x := []int {42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 
 	for i, v:= range x {
 		fmt.Println(i, v)
--- exercises/exercise-19.go
+++ exercises/exercise-19.go
@@ -13,7 +13,7 @@ package main
 import "fmt"
 
 func main()  {
-	x := []int {42, 43, 44, 45, 46, 47, 48, 49, 51, 52}
+	x := []int {42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 	fmt.Println(x[:5])
 	fmt.Println(x[5:])
 	fmt.Println(x[2:7])
