4e2126ed72207bf909e1dc40a0a232fbf3ed712e
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-02-20 09:18:49 +0100

    Channels range

---
+++ channels/examples/02_range/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+
+	for i := range c {
+		fmt.Println(i)
+	}
+}
