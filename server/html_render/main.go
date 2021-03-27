c08426fbc0ff21fa8d54fbc807884b0ba459c0d3
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-03-27 21:16:47 +0100

    Renderig html template

--- server/html_render/main.go
+++ server/html_render/main.go
@@ -6,7 +6,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+type Student struct {
+	Name string
+	College string
+}
+
 
 func home(w http.ResponseWriter, r *http.Request)  {
 	request := r.URL.Query()
@@ -17,8 +24,23 @@ func home(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 }
 
+func temp(w http.ResponseWriter, r *http.Request)  { 
+	student := Student{
+        Name:       "John Doe",
+        College:    "Stanford University",
+    }
+
+	parsedTemplate, _ := template.ParseFiles("template.html")
+    err := parsedTemplate.Execute(w, student)
+    if err != nil {
+        log.Println("Error executing template :", err)
+        return
+    }
+}
+
 func handleRequests() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/template", temp)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
