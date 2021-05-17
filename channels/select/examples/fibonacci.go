89068065a38b2500632d1035d73e769368c10dea
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-05-17 21:14:34 +0200

    fibonacci select example

---
+++ channels/select/examples/fibonacci.go
@@ -0,0 +1,43 @@
+// Example of using select instruction for fibonacci numbers
+
+package main 
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+			case c <- x:
+				x, y = y, x + y
+			case <-quit:
+				fmt.Println("quit")
+				return	
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	n := 10
+
+	if len(os.Args) == 2 {
+		n, _ = strconv.Atoi(os.Args[1])
+	}
+
+
+	go func (n int)  {
+		for i:= 0; i < n; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}(n)
+
+	fibonacci(c, quit)
+
+}
