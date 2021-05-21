333566ccbc58814b9fe5156d0fa47ecfe27f6204
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-05-21 21:29:40 +0200

    reading configuration of net interface

---
+++ net/examples/netConfig.go
@@ -0,0 +1,30 @@
+// Example of reading configuration of net interface
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main()  {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, i := range interfaces {
+		fmt.Printf("Interface: %v\n", i.Name)
+		byName, err := net.InterfaceByName(i.Name)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		address, err := byName.Addrs()
+		for k, v := range address {
+			fmt.Printf("Interface Address #%v: $v\n", k, v.String())
+		}
+
+		fmt.Println()
+	}
+}
