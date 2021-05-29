21cbd69fe9ed9e5f9056c32d4c5292a7acd34cb5
Radomir Brkovic <radomir.brkovic@pmf.edu.rs>
2021-05-29 18:11:59 +0200

    Other example of tcp server

---
+++ other/net/examples/servers/otherTcpServer.go
@@ -0,0 +1,63 @@
+// Other example of tcp server
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"string"
+)
+
+func main()  {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+
+	SERVER := "localhost:"+args[1]
+
+	s, err := net.ResloveTCPAddr("tcp", SERVER)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	l, err := net.ListenTCP("tcp", s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	buffer := make([]byte, 1024)
+	conn, err := l.Accept()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if string.TrimSpace(string(buffer[0:n])) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			conn.Close()
+			return
+		}
+
+		fmt.Print("> ", string(buffer[0:n-1]))
+		_, err = conn.Write(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+}
